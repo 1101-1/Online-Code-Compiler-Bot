14eb8256589f9d252332c273eecf68b3310a0c73
kir <k.melkozerov@gmail.com>
2023-05-18 22:25:58 +0500

    feat: Add python support

--- src/bot/python_code.rs
+++ src/bot/python_code.rs
@@ -1,9 +1,35 @@
 use teloxide::{requests::Requester, types::Message, Bot};
 
-use crate::types::state::{HandlerResult, MyDialogue, State};
+use crate::types::{state::{HandlerResult, MyDialogue, State}, json_response::{PythonPlayGroundRequest, PythonPlayGroundResponse}};
 
 pub async fn send_code(bot: Bot, msg: Message, dialogue: MyDialogue) -> HandlerResult {
-    bot.send_message(msg.chat.id, "Doesn't supported. Check /rust").await?;
-    dialogue.exit().await?;
+    if let Some(text) = msg.clone().text() {
+        let client = reqwest::Client::new();
+        let playground_req = serde_json::to_string(&PythonPlayGroundRequest {
+            code: text.to_string(),
+            codeld: None,
+            input: String::from(""),
+            language: String::from("py")
+        })
+        .unwrap();
+
+        let response = client
+            .post("https://api2.sololearn.com/v2/codeplayground/v2/compile")
+            .header("Content-Type", "application/json")
+            .body(playground_req)
+            .send()
+            .await?
+            .text()
+            .await?;
+        let playground_res: PythonPlayGroundResponse= serde_json::from_str(&response).unwrap();
+        let result = playground_res.data.output;
+        bot.send_message(msg.chat.id, format!("{}", result)).await?;
+        dialogue.exit().await?;
+        return Ok(());
+    }
+
+
+    bot.send_message(msg.chat.id, "Write a code, that you want to compile").await?;
+    dialogue.update(State::SendRustCode).await?;
     Ok(())
 }
--- src/types/json_response.rs
+++ src/types/json_response.rs
@@ -16,3 +16,33 @@ pub struct RustPlayGroundResponse {
     pub result: Option<String>,
     pub error: Option<String>,
 }
+
+#[derive(Serialize, Debug)]
+pub struct PythonPlayGroundRequest {
+    pub code: String,
+    pub codeld: Option<String>,
+    pub input: String,
+    pub language: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PythonPlayGroundResponse {
+    pub success: bool,
+    pub errors: Vec<String>,
+    pub data: Data,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    pub source_code: String,
+    pub status: i32,
+    pub error_code: i32,
+    pub error: Option<String>,
+    pub output_type: i32,
+    pub output: String,
+    pub output_style: Option<String>,
+    pub date: String,
+    pub language: String,
+    pub input: String,
+    pub id: i32,
+}
