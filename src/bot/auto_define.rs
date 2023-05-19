8fd511fe72223e30ffa4ff9fe5f994d7d1120d6d
kir <k.melkozerov@gmail.com>
2023-05-19 17:36:37 +0500

    typo

--- src/bot/auto_define.rs
+++ src/bot/auto_define.rs
@@ -7,7 +7,7 @@ use super::other_code;
 pub async fn auto_compile(bot: Bot, msg: Message, dialogue: MyDialogue) -> HandlerResult {
     if let Some(text) = msg.text() {
         let text = text.to_string();
-        if text.contains("#include <iostream>") {
+        if text.find("#include <iostream>").is_some() {
             bot.send_message(msg.chat.id, "Defined lang is C++. Sending result..")
                 .await?;
             other_code::send_code(
@@ -17,12 +17,12 @@ pub async fn auto_compile(bot: Bot, msg: Message, dialogue: MyDialogue) -> Handl
                 "cpp".to_string(),
             )
             .await?;
-        } else if text.contains("using System;") || text.contains("namespace") {
+        } else if text.find("using System;").is_some() || text.find("namespace").is_some() {
             bot.send_message(msg.chat.id, "Defined lang is C#. Sending result..")
                 .await?;
             other_code::send_code(bot.clone(), msg.clone(), dialogue.clone(), "cs".to_string())
                 .await?;
-        } else if text.contains("public static void") || text.contains("private static void") {
+        } else if text.find("public static void").is_some() || text.find("private static void").is_some() {
             bot.send_message(msg.chat.id, "Defined lang is Java. Sending result..")
                 .await?;
             other_code::send_code(
@@ -32,7 +32,7 @@ pub async fn auto_compile(bot: Bot, msg: Message, dialogue: MyDialogue) -> Handl
                 "java".to_string(),
             )
             .await?;
-        } else if text.contains("func") {
+        } else if text.find("func").is_some() {
             bot.send_message(msg.chat.id, "Defined lang is Go. Sending result..")
                 .await?;
             other_code::send_code(bot.clone(), msg.clone(), dialogue.clone(), "go".to_string())
